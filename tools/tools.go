37bb2640c59ca0854f6f6fb9713833360b65dd6a
Ruslan Sorokin <strawberryladder@gmail.com>
2023-09-03 14:35:10 +0300

    ci(Taskfile): add tasks to install tools

--- tools/tools.go
+++ tools/tools.go
@@ -5,7 +5,6 @@ package tools
 
 import (
 	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
-	_ "github.com/deepmap/oapi-codegen/pkg/codegen"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
