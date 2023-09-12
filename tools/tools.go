dbd88582a6d18f2a0efcec45698c325f25fc81ee
Ruslan Sorokin <strawberryladder@gmail.com>
2023-09-12 07:05:09 +0300

    chore(deps): add protolint

--- tools/tools.go
+++ tools/tools.go
@@ -5,6 +5,7 @@ package tools
 
 import (
 	_ "github.com/deepmap/oapi-codegen/cmd/oapi-codegen"
+	_ "github.com/yoheimuta/protolint/cmd/protolint"
 	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
